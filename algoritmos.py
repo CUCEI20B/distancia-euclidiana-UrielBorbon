769f0d02a345e66f6e53c217e325b9dd7100e076
Uriel Borbon <alfredo.medina4934@alumnos.udg.mx>
2020-10-15 00:31:38 -0500

    Actividad 9 Implementacion del Algoritmo (Distancia entre dos puntos)

--- algoritmos.py
+++ algoritmos.py
@@ -1,16 +1,12 @@
-def distancia_euclidiana(x_1, y_1, x_2, y_2):
-    """ Calcula la distancia euclidiana
+import math
 
-    Devuelve el resultado de la formula
 
-    También se le conoe a la fórmula como:
-    distancia entre dos puntos
+def distancetwopoints(origen_X, origen_Y, destino_X, destino_Y):
+    x_1 = origen_X
+    y_1 = origen_Y
+    x_2 = destino_X
+    y_2 = destino_Y
 
-    Parámetros:
-    x_1 -- origen_x
-    y_1 -- origen_y
-    x_2 -- destino_x
-    y_2 -- destino_y
+    d = math.sqrt((x_2 - x_1)**2 + (y_2 - y_1)**2)
 
-    """
-    
+    return d
