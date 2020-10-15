9df55ef95f81bde9537e61327c3aa26a90db1bb4
Uriel Borbon <alfredo.medina4934@alumnos.udg.mx>
2020-10-15 00:48:31 -0500

    Actividad 9 Test del Algoritmo de la Formula Euclidiana

--- algoritmos_test.py
+++ algoritmos_test.py
@@ -16,5 +16,5 @@ test_distancia2()
 def test_distancia3():
     assert algoritmos.distancetwopoints(500, 500, 200, 200) == 424.26406871192853
     print(algoritmos.distancetwopoints(500,500,200,200))
-    
+
 test_distancia3()
